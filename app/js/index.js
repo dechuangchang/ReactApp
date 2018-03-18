5466713865564fca97b05f07fd33064a45d09c52
常德闯 <changdechuang@gmail.com>
2018-03-18 15:25:41 +0800

    html less js

---
+++ app/js/index.js
@@ -0,0 +1,3 @@
+import add from './common/lib.js';
+import less from  '../less/index.less';
+let a = 1;
--- webpack.config.js
+++ webpack.config.js
@@ -11,64 +11,93 @@ var website = {
 website = {
     "publicPath":"/assets/img/"
 }
+var entry = {
+    index:'./app/js/index.js',
+    admin:'./app/js/admin.js',
+    jquery: "jquery"
+}
 module.exports = {
-    entry:{
-        one:'./src/build.js',
-        jquery: "jquery"
-    },//入口
+    entry:entry,//入口
     output:{
-        path:path.resolve(__dirname,'dist'),//输出真是硬盘位置
-        filename:'[name].js', //[name] 是用原文件名
-        //publicPath:'/temp/',//  开发环境虚拟内存并且在html路径修改为temp 实现热更新
+        path:path.resolve(__dirname,'output'),//输出真是硬盘位置
+        filename:'./js/[name].js', 
     },
     module:{
-        rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: "css-loader"
-            })
-        },{
-            test: /\.(png|jpg|gif)$/,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1,
-                        name:'[name]_[hash:6].[ext]',
-                        outputPath:'assets/img/',
-                        publicPath:website.publicPath
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        use:[{
+                        loader: "css-loader" 
+                        }, {
+                            loader: "less-loader" 
+                        }],
+                        fallback: "style-loader"
                     }
-                }
-            ]
-        },{
-            test: /\.html$/,
-            use: ['html-withimg-loader']
-        }]
+                )
+            },
+        // {
+        //     test: /\.(png|jpg|gif)$/,
+        //     use: [
+        //         {
+        //             loader: 'url-loader',
+        //             options: {
+        //                 limit: 1,
+        //                 name:'[name]_[hash:6].[ext]',
+        //                 outputPath:'assets/img/',
+        //                 publicPath:website.publicPath
+        //             }
+        //         }
+        //     ]
+        // },
+        // {
+        //     test: /\.html$/,
+        //     use: ['html-withimg-loader']
+        // }
+        ]
     },
     plugins:[
-        new webpack.ProvidePlugin({
-            $:'jquery'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "jquery",
-            minChunks: Infinity,
-            filename:"./js/jquery.js"
-        }),
-        new UglifyJSPlugin(),
+    //     new webpack.ProvidePlugin({
+    //         $:'jquery'
+    //     }),
+    //     new webpack.optimize.CommonsChunkPlugin({
+    //         name: "jquery",
+    //         minChunks: Infinity,
+    //         filename:"./js/jquery.js"
+    //     }),
+    //     new UglifyJSPlugin(),
         new HtmlWebpackPlugin({
             minify:{
                 removeAttributeQuotes:true
             },
             hash:true,
-            template:'./src/index.html'
+            filename: 'index.html',
+            chunks: ['index','jquery'],
+            template:'./app/index.html'
         }),
-        new ExtractTextPlugin("assets/css/[name].css"),
+        new HtmlWebpackPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            filename: 'admin.html',
+            chunks: ['admin','jquery'],
+            template:'./app/admin.html'
+        }),
+        new ExtractTextPlugin("./css/[name].css"),
     ],
-    devServer:{
-        contentBase:path.resolve(__dirname,'./dist/'), //服务器根路径
-        host:'127.0.0.1', //ip
-        compress:true, // 服务端压缩
-        port:'8002' // 端口
-    }
+    // devServer:{
+    //     contentBase:path.resolve(__dirname,'./dist/'), //服务器根路径
+    //     host:'127.0.0.1', //ip
+    //     compress:true, // 服务端压缩
+    //     port:'8002' // 端口
+    // }
 }
